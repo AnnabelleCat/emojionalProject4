340fb644fbdc282492d9d9ecee81821958b533fa
Apple <apple@Apples-MacBook-Pro-63.local>
2019-06-21 10:22:39 -0500

    I3 done

--- emojionalProject4/ViewController.swift
+++ emojionalProject4/ViewController.swift
@@ -15,13 +15,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+     let emojis = ["🥭🥥" : "mango coconut", "🍓🍌" : "strawberry banana", "🍈" : "melon", "🍎🥬": "apple spinach", "🍑🥑": "peach avacado", "🍉" : "watermelon" ]
+    
+    let customMessages = ["mango coconut": ["Good choice, this smoothie has a lot of Vitamin C", "Coconut is good for your skin!", "Copy this link: https://www.foodnetwork.com/recipes/food-network-kitchen/mango-coconut-and-chia-smoothie-3363058"], "strawberry banana": ["Bananas have a lot of potassium!", "This smoothie is rich in antioxidants!", "Copy this link: https://www.gimmesomeoven.com/strawberry-banana-smoothie-recipe/"], "melon": ["Honeydew is in season from August to October", "Honeydew has a high water content", "Copy this link:https://www.gimmesomeoven.com/strawberry-banana-smoothie-recipe/"], "apple spinach": ["Healthy choice!", "Spinach is a superfood!", "Copy this link: https://www.eatingonadime.com/apple-spinach-green-smoothie-recipe/"], "peach avacado": ["Avocados are a good source of fat", "Good choice!", "Copy this link:https://www.homemadenutrition.com/2016/05/19/green-avocado-peach-smoothie/"], "watermelon": ["This drink is very hydrating", "This is a refreshing summer drink!", "Copy this link: https://frommybowl.com/watermelon-slushie-recipe/"] ]
+    
+    
     @IBAction func showMessage(_sender: UIButton) {
-        let alertController = UIAlertController(title: "Alert", message: "vitamin C", preferredStyle: UIAlertController.Style.alert)
+        let selectedEmotion = _sender.titleLabel!.text!
+        let emojiMessage = customMessages[emojis[selectedEmotion]!]?[Int.random(in: 0..<3)]
+        
+        let alertController = UIAlertController(title: "Smoothie Tip", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
     }
+   
     
 }
 
